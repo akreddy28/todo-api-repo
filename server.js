89bb78ede6550dfbc1fd9a9053e8ab37b35ca08b
karunakar <akreddy888@gmail.com>
2016-01-29 19:51:45 +0530

    delete secnd postgres db

--- server.js
+++ server.js
@@ -181,6 +181,6 @@ app.put('/todos/:id', function(req, res) {
 
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
-		console.log('Express listening On ' + PORT + ' !');
+		console.log('Express Listening On ' + PORT + ' !');
 	});
 });
