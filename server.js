492f300ebb76d6cc86bb58da8572634fee02fa98
karunakar <akreddy888@gmail.com>
2016-01-29 14:04:43 +0530

    get by id

--- server.js
+++ server.js
@@ -44,21 +44,32 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
-	// var matchedTodo;
-	// todos.forEach(function (todo) {
-	// 	if(todoId === todo.id) {
-	// 		matchedTodo = todo;
-	// 	}
-	// });
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function(todo) {
+		if(!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
+
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+	// // var matchedTodo;
+	// // todos.forEach(function (todo) {
+	// // 	if(todoId === todo.id) {
+	// // 		matchedTodo = todo;
+	// // 	}
+	// // });
+
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 });
 
 
