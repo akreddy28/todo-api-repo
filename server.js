47ead1fe0f78ae642f5bc95559bd1460a511b65a
karunakar <akreddy888@gmail.com>
2016-02-01 14:59:38 +0530

    add lowercase validation to email

--- server.js
+++ server.js
@@ -110,11 +110,10 @@ app.post('/user', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(todo.toJSON());
+		res.json(user.toJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
-
 });
 
 db.sequelize.sync().then(function() {
